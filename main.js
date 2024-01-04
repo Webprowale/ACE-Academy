f0914ee126f82cfa984bf1700994a0856bc27da0
Webprowale <webprowale@gmail.com>
2024-01-04 13:23:00 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+gsap.to(".doText",{
+        duration:1,
+        opacity:2,
+        ease: "power1",
+        yoyo:true,
+        repeat:-1,
+        stagger:{
+            amount:4,
+            each:0.5,
+            from:0,
+        }
+
+})
