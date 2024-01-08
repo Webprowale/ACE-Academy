1b05b4d4a3036471443b6caa235892256538491a
Webprowale <webprowale@gmail.com>
2024-01-08 07:29:03 -1100

    landing page design

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-gsap.to(".doText",{
-        duration:1,
-        opacity:2,
-        ease: "power1",
-        yoyo:true,
-        repeat:-1,
-        stagger:{
-            amount:4,
-            each:0.5,
-            from:0,
-        }
-
-})
+$('#myCarousel2').carousel({
+    interval: 8000
+  });
+  
